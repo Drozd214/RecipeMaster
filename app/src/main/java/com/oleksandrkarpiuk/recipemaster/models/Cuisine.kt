18d4e671c354451424628efba55f31797f8a9879
Oleksandr <o.karpiuk@coros.net>
2022-01-10 12:07:12 +0200

    Added enums for MealType, Diets and Cuisines

---
+++ app/src/main/java/com/oleksandrkarpiuk/recipemaster/models/Cuisine.kt
@@ -0,0 +1,25 @@
+package com.oleksandrkarpiuk.recipemaster.models
+
+enum class Cuisine(name: String) {
+
+    AFRICAN("african"),
+    AMERICAN("american"),
+    BRITISH("british"),
+    CHINESE("chinese"),
+    EASTERN_EUROPEAN("eastern european"),
+    EUROPEAN("european"),
+    FRENCH("french"),
+    GERMAN("german"),
+    GREEK("greek"),
+    INDIAN("indian"),
+    ITALIAN("italian"),
+    JAPANESE("japanese"),
+    LATIN_AMERICAN("latin american"),
+    MEDITERRANEAN("mediterranean"),
+    MEXICAN("mexican"),
+    NORDIC("nordic"),
+    SOUTHERN("southern"),
+    SPANISH("spanish"),
+    VIETNAMESE("vietnamese")
+
+}
---
+++ app/src/main/java/com/oleksandrkarpiuk/recipemaster/models/Diet.kt
@@ -0,0 +1,12 @@
+package com.oleksandrkarpiuk.recipemaster.models
+
+enum class Diet(name: String) {
+
+    GLUTEN_FREE("gluten free"),
+    KETOGENIC("ketogenic"),
+    VEGETARIAN("vegetarian"),
+    LACTO_VEGETARIAN("lacto-vegetarian"),
+    OVO_VEGETARIAN("ovo-vegetarian"),
+    VEGAN("vegan")
+
+}
